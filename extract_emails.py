3525bdf52c9fb0b33d5d8a1e0f7eb9834081093f
Billymonteforte14 <billymonteforte14@gmail.com>
2025-02-27 18:50:27 -0500

    Delete extract_emails.py

--- extract_emails.py
+++
@@ -1,9 +0,0 @@
-import re
-
-text = "Contact us at support@example.com or sales@example.org for assistance."
-
-email_pattern = r'[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}'
-
-emails = re.findall(email_pattern, text)
-
-print(emails)
